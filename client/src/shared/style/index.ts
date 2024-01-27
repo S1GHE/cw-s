be55b554f6818476d25ff22f64c477a193e7e18c
S1GHE <golikovefim65@gmail.com>
2024-01-27 15:00:20 +0300

    ADD: module style (text/container/func)

---
+++ client/src/shared/style/index.ts
@@ -0,0 +1,6 @@
+import ContainerStyle from "src/shared/style/container/container.module.scss";
+import TextStyle from "src/shared/style/text/text.module.scss";
+
+export {
+    ContainerStyle, TextStyle
+}
